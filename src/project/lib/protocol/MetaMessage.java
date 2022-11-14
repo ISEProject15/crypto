55a89086a23b7fd809a1bb3bb964cf829b8524aa
maemon4095 <maemondecocted@gmail.com>
2022-11-14 10:21:50 +0900

    metamessage

--- src/project/lib/protocol/MetaMessage.java
+++ src/project/lib/protocol/MetaMessage.java
@@ -1,15 +1,7 @@
 package project.lib.protocol;
 
-public class MetaMessage {
-    public static MetaMessage parse(String text) {
+public interface MetaMessage {
+    public String identity();
 
-    }
-
-    public final String identity;
-    public final MessageBody body;
-
-    private MetaMessage(String identity, MessageBody body) {
-        this.identity = identity;
-        this.body = body;
-    }
+    public MessageBody body();
 }
