9d3154a49e43480ebae5a0f08f61ddacdd5f4416
maemon4095 <maemondecocted@gmail.com>
2022-11-16 10:47:14 +0900

    encrypter

--- src/project/lib/crypto/RSAEncrypter.java
+++ src/project/lib/crypto/RSAEncrypter.java
@@ -3,8 +3,13 @@ package project.lib.crypto;
 import java.math.BigInteger;
 
 public class RSAEncrypter implements project.lib.Transformer {
-    private final BigInteger module;
+    private final BigInteger modulo;
     private final BigInteger exponent;
+
+    private RSAEncrypter(BigInteger modulo, BigInteger exponent) {
+        this.modulo = modulo;
+        this.exponent = exponent;
+    }
 
     @Override
     public int transform(int sourceLength, byte[] source, byte[] destination) {
