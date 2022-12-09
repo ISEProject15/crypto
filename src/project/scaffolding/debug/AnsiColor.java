cd9879ebc0487484178cffa02e6fb4a85bccc956
maemon <47819358+maemon4095@users.noreply.github.com>
2022-12-09 00:59:05 +0000

    ansi

--- src/project/scaffolding/debug/AnsiColor.java
+++ src/project/scaffolding/debug/AnsiColor.java
@@ -1,10 +1,45 @@
 package project.scaffolding.debug;
 
+import java.util.function.BooleanSupplier;
+
 public class AnsiColor {
-    static final String SET_COLOR = "\u001B[";
-    static final String RESET = "0m";
-    static final String COLOR_RESET = SET_COLOR + RESET;
-    static final String BG_RED = "41m";
-    static final String BG_GREEN = "42m";
-    static final String BG_BLUE = "44m";
+    private static final String PREFIX = "\u001B[";
+
+    private static final byte BG = 1;
+    private static final byte FG = 2;
+    private static final byte MASK = 3;
+
+    public static final AnsiColor reset = new AnsiColor("0m", BG | FG);
+
+    public static final AnsiColor bgBlack = new AnsiColor("40", BG);
+    public static final AnsiColor bgRed = new AnsiColor("41", BG);
+    public static final AnsiColor bgGreen = new AnsiColor("42", BG);
+    public static final AnsiColor bgBlue = new AnsiColor("44", BG);
+
+    private AnsiColor(String color, byte flags) {
+        this.color = color;
+        this.flags = flags;
+    }
+
+    private final String color;
+    private final byte flags;
+
+    public AnsiColor combine(AnsiColor other) {
+        if (this.isCombined() || other.isCombined()) {
+            throw new IllegalStateException();
+        }
+        return new AnsiColor(this.color + ";" + other.color + "m");
+    }
+
+    public boolean isCombined() {
+        return this.color.endsWith("m");
+    }
+
+    @Override
+    public String toString() {
+        if (this.isCombined()) {
+            return PREFIX + color;
+        }
+        return PREFIX + color + "m";
+    }
 }
